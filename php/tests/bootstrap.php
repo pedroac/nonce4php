598f9873a501a786ac753e7a34b71dc734af19a5
Pedro Amaral Couto <pedro.amaral.couto@gmail.com>
2018-03-30 00:11:33 +0100

    Add the PHPUnit configuration.

---
+++ php/tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author pedroac
+ */
+require_once __DIR__ . '/../vendor/autoload.php';
